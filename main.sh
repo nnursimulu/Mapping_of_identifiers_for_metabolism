a7d9b643b587c7833a7acab8d71719dbd43c4a9d
U-ISOSPORA2\Nirvana <anavrin.ulumisrun@gmail.com>
2023-04-17 11:23:24 -0400

    Initial logic for what to download, parse etc from KEGG/BiGG in terms of reaction definitions

--- main.sh
+++ main.sh
@@ -11,5 +11,5 @@ mkdir -p ${PARSED_MET_SPLIT_INFO}
 #python3 scripts/0_fetch_metanetx_data.py --output-folder $OUTSIDE_INFO
 #python3 scripts/1_parse_out_identifier_map.py --metanetx-folder $OUTSIDE_INFO --parsed-info $PARSED_INFO
 #python3 scripts/2_create_individual_db_map.py --parsed-info $PARSED_INFO --parsed-split-rxn-info $PARSED_RXN_SPLIT_INFO --parsed-split-met-info $PARSED_MET_SPLIT_INFO
-python3 scripts/3_parse_out_rxn_definitions.py
+python3 scripts/3_parse_out_rxn_definitions.py --output-folder $OUTSIDE_INFO --parsed-info $PARSED_INFO
 #python3 scripts/4_determine_rxn_reversibility_equiv.py --parsed-info $PARSED_INFO
--- scripts/3_parse_out_rxn_definitions.py
+++ scripts/3_parse_out_rxn_definitions.py
@@ -1,9 +1,27 @@
 # Author: Nirvana Nursimulu
 
 from argparse import ArgumentParser
-import utils
+import utils, subprocess
 
 if __name__ == '__main__':
 
-    parser = ArgumentParser(description="Parse out reaction definitions from MetaNetX.")
-    # TODO
+    parser = ArgumentParser(description="Parse out reaction definitions for KEGG and BiGG.")
+    parser.add_argument("--output-folder", type=str, help="Folder to contain downloaded info from BiGG.")
+    parser.add_argument("--parsed-info", type=str, help="Folder that contains the parsed info from KEGG and BiGG.")
+
+    args = parser.parse_args()
+    output_folder = args.output_folder
+    parsed_info = args.parsed_info
+
+    kegg_output_file = parsed_info + "/KEGG_rxn_equations.out"
+    #TODO: download parsed out KEGG reaction equations from the website.
+
+    # For BiGG, first download the file with the information.
+    bigg_initial_download = output_folder + "/bigg_models_reactions.txt"
+    url_bigg = "http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt"
+    try:
+        subprocess.call(["wget", url_bigg, "-O", bigg_initial_download])
+    except Exception:
+        print ("Error: could not download from http://bigg.ucsd.edu/static/namespace/bigg_models_reactions.txt for some reason.")
+    
+    # TODO: for BiGG, parse out the reaction definition.
